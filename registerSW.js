693c1a984924fd84615dc344f0a98c265d5abb0c
FaridBayu <mfaridbs@gmail.com>
2025-11-10 13:53:31 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Pets_Cerita/sw.js', { scope: '/Pets_Cerita/' })})}
